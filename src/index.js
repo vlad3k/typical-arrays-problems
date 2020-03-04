01786094fcf1532bbcbef962c0aeab9a6ce9fc99
poezhaev.vs <poezhaev.vs@esphere.ru>
2020-03-04 11:21:44 +0300

    feat: solve task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
+  if(array && array.length ) {
+    return Math.min(...array);
+  }
   return 0;
-}
+};
 
 exports.max = function max (array) {
+  if (array && array.length ) {
+    return Math.max(...array);
+  }
   return 0;
-}
+};
 
 exports.avg = function avg (array) {
+  if (array && array.length) {
+    return array.reduce((acc, num) => acc + num, 0) / array.length;
+  }
   return 0;
-}
+};
